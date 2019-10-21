978501f2b26cf4adf24f89ab099667995322ed07
unknown <hoaithu.taos@gmail.com>
2019-10-21 08:38:35 +0700

    add file

--- js/freetrial.js
+++ js/freetrial.js
@@ -1,12 +1,24 @@
 $(document).ready(function () {
     $('.free-trial__step-one .type-list .type-item').click(function () {
-        $('.free-trial__step-two').show();
-        $('.free-trial__step-one').css("padding-bottom","0");
+        showStep('.free-trial__step-two')
+        $('.free-trial__step-one').css("padding-bottom", "0");
+        smoothScroll('.free-trial__step-two');
+        var animalType = $('.type-item').getAttribute("data-type");
+        console.log(animalType);
     });
 
     $('.free-trial__step-two .type-list .type-item').click(function () {
-        $('.free-trial__step-three').show();
-        $('.free-trial__step-two').css("padding-bottom","0");
+        showStep('.free-trial__step-three')
+        $('.free-trial__step-two').css("padding-bottom", "0");
+        smoothScroll('.free-trial__step-three')
     });
 
 })
+function showStep(e) {
+    $(e).show();
+}
+function smoothScroll(e) {
+    $('html, body').animate({
+        scrollTop: $(e).offset().top
+    }, 800);
+}
