8bf1785220ee28b7894e1368376c7354a8d78dac
unknown <hoaithu.taos@gmail.com>
2019-10-21 09:30:11 +0700

    add file

--- js/freetrial.js
+++ js/freetrial.js
@@ -1,19 +1,25 @@
 $(document).ready(function () {
+    // show step 2
     $('.free-trial__step-one .type-list .type-item').click(function () {
         showStep('.free-trial__step-two')
         $('.free-trial__step-one').css("padding-bottom", "0");
-        smoothScroll('.free-trial__step-two');
-        var animalType = $('.type-item').getAttribute("data-type");
-        console.log(animalType);
+        smoothScroll('.free-trial__step-two'); // scroll to top step 2
+        var type = (this).getAttribute('data-type');
+        document.getElementById("business-type").value = type;
     });
 
+    //show step 3
     $('.free-trial__step-two .type-list .type-item').click(function () {
         showStep('.free-trial__step-three')
         $('.free-trial__step-two').css("padding-bottom", "0");
-        smoothScroll('.free-trial__step-three')
+        smoothScroll('.free-trial__step-three') // scroll to top step 3
+        var number = (this).getAttribute('data-number');
+        document.getElementById("num-store").value = number;
+        //alert(number);
     });
 
 })
+
 function showStep(e) {
     $(e).show();
 }
