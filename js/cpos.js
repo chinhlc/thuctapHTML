149a5ea1d42a7b4832f7aea0570281c5715b68dd
unknown <hoaithu.taos@gmail.com>
2019-10-24 11:55:39 +0700

    add file

--- js/cpos.js
+++ js/cpos.js
@@ -1,7 +1,34 @@
 
-$(document).ready(function(){
-    $('.menu-parent__link').click(function(){
+$(document).ready(function () {
+    $('.menu-parent__link').click(function () {
         //console.log('true');
-
+        var number = (this).getAttribute('data-number');
+        //console.log(number);
+        switch(number) {
+            case "1": smoothScroll('#product', 130);
+            break;
+            case "2": smoothScroll('#checkout', 50);
+            break;
+            case "3": smoothScroll('#payment', 50);
+            break;
+            case "4": smoothScroll('#customer', 50);
+            break;
+            case "5": smoothScroll('#report', 50);
+            break;
+            case "6": smoothScroll('#store', 50);
+            break;
+        }
+        //changePage(this, '.change-page')
+        //smoothScroll('#product')
+        $(this).siblings().removeClass('change-page');
+        $(this).addClass('change-page');
+        //styleClick(this,'.change-page')
     })
+    //var pos = $('#checkout').position();
+    //console.log(pos.top);
 })
+/*function smoothScrollFirst(e) {
+    $('html, body').animate({
+        scrollTop: $(e).offset().top-130
+    }, 1000);
+}*/
--- js/freetrial.js
+++ js/freetrial.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     $('.free-trial__step-one .type-list .type-item').click(function () {
         showStep('.free-trial__step-two')
         $('.free-trial__step-one').css("padding-bottom", "0");
-        smoothScroll('.free-trial__step-two'); // scroll to top step 2
+        smoothScroll('.free-trial__step-two', 0); // scroll to top step 2
         var type = (this).getAttribute('data-type');
         document.getElementById("business-type").value = type;
         styleClick(this, '.type-item__content') // change style when click
@@ -13,7 +13,7 @@ $(document).ready(function () {
     $('.free-trial__step-two .type-list .type-item').click(function () {
         showStep('.free-trial__step-three')
         $('.free-trial__step-two').css("padding-bottom", "0");
-        smoothScroll('.free-trial__step-three') // scroll to top step 3
+        smoothScroll('.free-trial__step-three', 0) // scroll to top step 3
         var number = (this).getAttribute('data-number');
         document.getElementById("num-store").value = number;
         //alert(number);
@@ -25,9 +25,9 @@ $(document).ready(function () {
 function showStep(e) {
     $(e).show();
 }
-function smoothScroll(e) {
+function smoothScroll(e, number) {
     $('html, body').animate({
-        scrollTop: $(e).offset().top
+        scrollTop: $(e).offset().top-number
     }, 800);
 }
 function styleClick(e, path) {
