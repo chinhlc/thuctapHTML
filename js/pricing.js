889693dbb5d5f68b4062a06905bf3297d9a5be99
unknown <hoaithu.taos@gmail.com>
2019-11-01 01:22:01 +0700

    fix file

--- js/pricing.js
+++ js/pricing.js
@@ -4,13 +4,13 @@ $(document).ready(function () {
     $('.check-box').click(function () {
         count = (count + 1) % 2;
         $('.pricing-table').toggleClass('pricing-action');
-        $('.box-price__monney').each(function(){
+        $('.box-price__monney').each(function () {
             var number = (this).getAttribute('data-target');
-            if(count === 0) $(this).html(number - 10);
+            if (count === 0) $(this).html(number - 10);
             else $(this).html(number);
         })
     })
-    $('.card').click(function() {
+    $('.card').click(function () {
         $(this).toggleClass('question-shadow');
         $(this).siblings().removeClass('question-shadow');
     })
